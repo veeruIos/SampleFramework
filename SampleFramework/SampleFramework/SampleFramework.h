919e26a06c30856637ccd9499642fce5e087b30e
Manohar <manohar@Manohars-MBP.attlocal.net>
2023-03-16 22:01:59 -0500

    SampleProject

---
+++ SampleFramework/SampleFramework/SampleFramework.h
@@ -0,0 +1,18 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Manohar on 3/16/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
---
+++ SampleFramework/SampleFramework/Services.swift
@@ -0,0 +1,17 @@
+//
+//  Services.swift
+//  SampleFramework
+//
+//  Created by Manohar on 3/16/23.
+//
+
+import Foundation
+
+public class Services {
+    
+    private init() {}
+    
+    public static func frameworkName() -> String {
+        return "MQTT"
+    }
+}
